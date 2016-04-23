633c24a1e6eb3b96f39bc76eac4c8aec60108387
Olov Johansson <johansson.olov@gmail.com>
2016-04-23 15:36:07 +0200

    Added Atom and byobo

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # erlang
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+rm erlang-solutions_1.0_all.deblang-solutions_1.0_all.deb
 sudo apt-get update
 sudo apt-get install esl-erlang
 
 # misc
 sudo apt-get install git
-sudo apt-get install vim
 
 # postgres
 sudo apt-get install postgresql postgresql-contrib
@@ -21,3 +21,12 @@ sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo apt-get install elixir
 mix local.hex
 mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
+
+# Editors
+wget https://github.com/atom/atom/releases/download/v1.7.2/atom-amd64.deb
+sudo dpkg -i atom-amd64.deb
+rm atom-amd64.deb
+sudo apt-get install vim
+
+# Byobu
+sudo apt-get install byobu
